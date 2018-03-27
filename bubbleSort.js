d2239d18e340cceacc60ef215dfd1f5ffac4c39b
suhong <1067489058@qq.com>
2018-03-27 16:38:42 +0800

    冒泡排序和选择排序

---
+++ bubbleSort.js
@@ -0,0 +1,33 @@
+/*
+	冒泡排序：
+		时间复杂度：N + N-1 + N - 2 + .... + 1
+		        = N(1 + N)/2
+		        = N + N^2/2
+		        = N^2
+		        = big O(1) * N^2
+		        = O(N^2)
+
+				Sn = n(a1 + an)/2
+				   = n*a1 + n(n-1)d/2
+				   
+		最好情况时间复杂度：O(n)
+		最坏情况时间复杂度：O(n^2)
+
+		空间复杂度：O(1)
+*/
+console.log( bubbleSort([3, 2, 7, 5, 0]) );
+function bubbleSort(arr) {
+	if ( arr == null || arr.length < 2 ) {
+		return;
+	}
+	for ( var end = arr.length - 1; end > 0; end -- ) {
+		for ( var i = 0; i < end; i ++ ) {
+			if ( arr[i] > arr[i + 1] ) {
+				var temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
+			}
+		}
+	}
+	return arr;
+}
---
+++ selectSort.js
@@ -0,0 +1,27 @@
+/*
+  选择排序：
+    时间复杂度：O(n^2)
+           
+    最好情况时间复杂度：O(n^2)
+    最坏情况时间复杂度：O(n^2)
+
+    空间复杂度：O(1)
+*/
+console.log( bubbleSort([3, 2, 7, 5, 0]) );
+
+
+function selectSort(arr) {
+  if ( arr == null || arr.length < 2 ) {
+    return;
+  }
+  for ( var i = 0; i < arr.length; i ++ ) {
+    var minIndex = i;
+    for ( var j = i + 1; j < arr.length; j ++ ) {
+      minIndex = arr[i] > arr[j] ? j : minIndex;
+    }
+    var temp = arr[minIndex];
+    arr[minIndex] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
+}
