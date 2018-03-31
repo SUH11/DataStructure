5aefa790951ad60906471fe4154c1b7f23dace26
suhong <1067489058@qq.com>
2018-03-31 20:25:07 +0800

    堆排序，有点问题

---
+++ heapSort.js
@@ -0,0 +1,61 @@
+/*
+	堆排序
+		时间复杂度：O(nlogn)
+		空间复杂度：O(logn)
+*/
+
+var arr = [1, 6, 9, 4, 3, 2];
+console.log( heapSort(arr) );
+
+function heapSort(arr) {
+	if ( arr == null || arr.length < 2 ) {
+		return;
+	}
+	for ( var i = 0; i < arr.length; i ++ ) {
+		arr = heapInsert(arr, i);
+	}
+	var heapSize = arr.length - 1;
+
+	var temp = arr[0];
+	arr[0] = arr[heapSize];
+	arr[heapSize] = temp;
+
+	while( heapSize > 0 ) {
+		arr = heapify(arr, 0, heapSize);
+		var wrap = arr[0];
+		arr[0] = arr[--heapSize];
+		arr[heapSize] = wrap;
+	}
+
+	return arr;
+}
+
+// 构建一个堆
+function heapInsert(arr, index) {
+	while( arr[index] > arr[parseInt((index - 1) / 2)] ) {
+		var temp = arr[index];
+		arr[index] = arr[parseInt((index - 1) / 2)];
+		arr[parseInt( (index - 1) / 2 )] = temp;
+		index = parseInt((index - 1) / 2);
+	}
+	return arr;
+}
+
+// 堆中，往下沉
+function heapify(arr, index, heapSize) {
+	var left = index * 2 + 1;
+	while( left < heapSize ) {
+		var largest = left + 1 < heapSize && arr[left + 1] > arr[left] ? left + 1 : left;
+		largest = largest > index ? largest : index;
+		if ( largest == index ) {
+			break;
+		}
+		var temp = arr[largest];
+		arr[largest] = arr[index];
+		arr[index] = temp;
+
+		index = largest;
+		left = index * 2 + 1;
+	}
+	return arr;
+}
